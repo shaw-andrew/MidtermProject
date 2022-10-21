9c0072884dcb0d6687e2cc5753a42cfcdea727e3
rubenarocha14 <rubenarocha14@gmail.com>
2022-10-21 16:37:25 -0600

    1/2 tests created

--- JPACMMS/src/main/java/com/skilldistillery/cmms/entities/MaintenanceRequirementCard.java
+++ JPACMMS/src/main/java/com/skilldistillery/cmms/entities/MaintenanceRequirementCard.java
@@ -5,7 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Table(name = "maintenance_requirement_card")
 @Entity
 public class MaintenanceRequirementCard {
 
