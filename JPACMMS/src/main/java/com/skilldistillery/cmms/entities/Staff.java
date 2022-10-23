5eeb9a837426265c9c98743dbc84b0e61864d2c9
Andrew Shaw <shandyshawshank@gmail.com>
2022-10-23 09:52:19 -0600

    resolved merge conflict

--- JPACMMS/src/main/java/com/skilldistillery/cmms/entities/Staff.java
+++ JPACMMS/src/main/java/com/skilldistillery/cmms/entities/Staff.java
@@ -10,7 +10,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -47,108 +46,95 @@ public class Staff {
 	@ManyToMany(mappedBy = "staff")
 	private List<Certification> certifications;
 	
-	@OneToMany(mappedBy = "staff")
-	private List<Staff> staff;
+//	@OneToMany(mappedBy = "staff")
+//	private List<Staff> staff;
 
 	public Staff() {
 	}
-	
-	
 
-	public Staff(int id, String firstName, String lastName, Location location, Integer managerId,
-			Location supervisedLocation, User user, List<Certification> certifications, List<Staff> staff) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.location = location;
-		this.managerId = managerId;
-		this.supervisedLocation = supervisedLocation;
-		this.user = user;
-		this.certifications = certifications;
-		this.staff = staff;
-	}
+public Staff(int id, String firstName, String lastName, Location location, Integer managerId,
+		Location supervisedLocation, User user, List<Certification> certifications) {
+	super();
+	this.id = id;
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.location = location;
+	this.managerId = managerId;
+	this.supervisedLocation = supervisedLocation;
+	this.user = user;
+	this.certifications = certifications;
+}
 
-	public int getId() {
-		return id;
-	}
+public int getId() {
+	return id;
+}
 
-	public void setId(int id) {
-		this.id = id;
-	}
+public void setId(int id) {
+	this.id = id;
+}
 
-	public String getFirstName() {
-		return firstName;
-	}
+public String getFirstName() {
+	return firstName;
+}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
 
-	public String getLastName() {
-		return lastName;
-	}
+public String getLastName() {
+	return lastName;
+}
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
 
-	public Location getLocation() {
-		return location;
-	}
+public Location getLocation() {
+	return location;
+}
 
-	public void setLocation(Location location) {
-		this.location = location;
-	}
+public void setLocation(Location location) {
+	this.location = location;
+}
 
-	public Integer getManagerId() {
-		return managerId;
-	}
+public Integer getManagerId() {
+	return managerId;
+}
 
-	public void setManagerId(Integer managerId) {
-		this.managerId = managerId;
-	}
+public void setManagerId(Integer managerId) {
+	this.managerId = managerId;
+}
 
-	public Location getSupervisedLocation() {
-		return supervisedLocation;
-	}
+public Location getSupervisedLocation() {
+	return supervisedLocation;
+}
 
-	public void setSupervisedLocation(Location supervisedLocation) {
-		this.supervisedLocation = supervisedLocation;
-	}
+public void setSupervisedLocation(Location supervisedLocation) {
+	this.supervisedLocation = supervisedLocation;
+}
 
-	public User getUser() {
-		return user;
-	}
+public User getUser() {
+	return user;
+}
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+public void setUser(User user) {
+	this.user = user;
+}
 
-	public List<Certification> getCertifications() {
-		return certifications;
-	}
+public List<Certification> getCertifications() {
+	return certifications;
+}
 
-	public void setCertifications(List<Certification> certifications) {
-		this.certifications = certifications;
-	}
+public void setCertifications(List<Certification> certifications) {
+	this.certifications = certifications;
+}
 
-	public List<Staff> getStaff() {
-		return staff;
-	}
-
-	public void setStaff(List<Staff> staff) {
-		this.staff = staff;
-	}
-
-	@Override
-	public String toString() {
-		return "Staff [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", location=" + location
-				+ ", managerId=" + managerId + ", supervisedLocation=" + supervisedLocation + ", user=" + user
-				+ ", certifications=" + certifications + ", staff=" + staff + "]";
-	}
-
-	
-	
+@Override
+public String toString() {
+	return "Staff [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", location=" + location
+			+ ", managerId=" + managerId + ", supervisedLocation=" + supervisedLocation + ", user=" + user
+			+ ", certifications=" + certifications + "]";
+}
 	
 }
+	
