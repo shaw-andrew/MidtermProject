d6fa814bc061f0db0731c7c2a5f3188b42dc1137
Ethan Lauzon <ethan.lauzon@gmail.com>
2022-10-26 15:31:17 -0600

    configure app for war deployment to tomcat

--- CMMS/src/main/java/com/skilldistillery/cmms/data/MaintenanceDetailDAOImpl.java
+++ CMMS/src/main/java/com/skilldistillery/cmms/data/MaintenanceDetailDAOImpl.java
@@ -7,7 +7,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
-import org.apache.log4j.chainsaw.Main;
 import org.springframework.stereotype.Service;
 
 import com.skilldistillery.cmms.entities.Location;
